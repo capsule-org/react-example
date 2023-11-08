91f98d2e494386ebf5ec6e1892437809c9725c76
Collin Jackson <github@collinjackson.com>
2023-11-07 19:03:49 -0800

    Fix link

--- src/nft-marketplace/components/navbar/Navbar.jsx
+++ src/nft-marketplace/components/navbar/Navbar.jsx
@@ -6,7 +6,7 @@ import { Link } from "react-router-dom";
 
 const Menu = () => (
   <>
-    <Link target="_blank" rel="noreferrer" to='https://github.com/capsule-org/capsule-example'><p>Demo</p></Link>
+    <Link target="_blank" rel="noreferrer" to='https://demo.beta.usecapsule.com'><p>Demo</p></Link>
     <Link target="_blank" rel="noreferrer" to='https://docs.usecapsule.com/'><p>Docs</p></Link>
     <Link target="_blank" rel="noreferrer" to='https://7f4shq8oyfd.typeform.com/to/F86oVLhb'><p class='cta'>Get Access</p></Link>
   </>
