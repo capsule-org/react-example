df2b011b6602375bade7c0c78e62ecb2ac76d3a2
Nitya Subramanian <nitya@usecapsule.com>
2023-11-08 23:46:31 -0800

    add default API key

--- src/nft-marketplace/pages/home/Home.jsx
+++ src/nft-marketplace/pages/home/Home.jsx
@@ -3,6 +3,9 @@ import { NFT, Header, } from '../../components'
 import Capsule, { Environment } from "@usecapsule/web-sdk";
 import './home.css'
 
+// Get an api key at usecapsule.com
+const TEST_API_KEY = "d0b61c2c8865aaa2fb12886651627271";
+
 const Home = () => {
   const environment = Environment.DEVELOPMENT;
 
