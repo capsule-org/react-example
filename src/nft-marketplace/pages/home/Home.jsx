8d61dbffd33731c9a3612a26e959421777ef709b
Collin Jackson <github@collinjackson.com>
2023-11-08 17:00:30 -0800

    Remove footer

--- src/nft-marketplace/pages/home/Home.jsx
+++ src/nft-marketplace/pages/home/Home.jsx
@@ -16,14 +16,6 @@ const Home = () => {
         <NFT environment={environment} capsule={capsule} />
       </div>
     </div>
-    <div className="footer">
-      <p>
-        This is a test application to illustrate Capsule's SDK and
-        is intended to be used only for testing purposes. Assets used herein are on
-        the Sepolia testnet and valueless. Additionally, this demo app is connected
-        to a beta environment and test accounts created may be occasionally deleted.
-      </p>
-    </div>
   </>
 };
 
