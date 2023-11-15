ad7dda454bf2ca9f0fa656e742374320d5c02600
Collin Jackson <github@collinjackson.com>
2023-11-15 08:34:55 -0800

    Add a footer to provide spacing

--- src/nft-marketplace/pages/home/Home.jsx
+++ src/nft-marketplace/pages/home/Home.jsx
@@ -11,22 +11,20 @@ const Home = () => {
   });
 
   return <main className="main-page-content tuck-below-header">
-  <section className="section-about-hero theme-dark">
+    <section className="section-about-hero theme-dark">
       <div className="section-content" style={{maxWidth: '41em'}}>
         <NFT environment={environment} capsule={capsule} />
       </div>
 
       <img className="sunrise-graphic img-loaded" src="https://assets.codepen.io/44800/capsule-sunrise-about-hero.svg?" alt=""></img>
 
-      <section class="section-about-values">
-        <div class="section-content">
-
-          <h2 class="text-gradient-bidirectional" style={{paddingBottom: '1000px' /* Gradient Clip Fix */}}>
-          </h2>
-
-        </div>
-      </section>
-          </section>
+    </section>
+    <footer class="site-footer theme-dark">
+      <div class="site-footer__content">
+        <section class="site-footer__featured-area">
+        </section>
+      </div>
+    </footer>
   </main>;
 }
 
