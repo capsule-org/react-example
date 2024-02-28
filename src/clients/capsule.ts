692ed54c066cacf4e98e76226f2fa4cd9cc977c0
Taylor Bosch <43397354+tbosch82@users.noreply.github.com>
2024-02-27 20:48:34 -0700

    update capsule env

--- src/clients/capsule.ts
+++ src/clients/capsule.ts
@@ -1,9 +1,10 @@
 import { Environment } from "@usecapsule/web-sdk";
 import Capsule from "@usecapsule/web-sdk";
+import { isProd } from "../constants";
 
 // Get an api key at usecapsule.com
 const TEST_API_KEY = "d0b61c2c8865aaa2fb12886651627271";
 
-export const environment = Environment.DEVELOPMENT;
+export const environment = isProd ? Environment.PROD : Environment.DEVELOPMENT;
 
 export const capsule = new Capsule(environment, TEST_API_KEY);
